1815c10c27c30919b8e6583d5baaae0d9e9c2216
Matheus <matheus.peres01@fatec.sp.gov.br>
2018-04-30 20:31:00 -0300

    Coreção dia 31-04-2018

---
+++ Exemplos_Aula_C/Leticia.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <time.h>
+
+int i, x;
+
+void main(void){
+	
+		srand(time(NULL));
+		for(;x;){
+			
+			x= (rand()%11)-5;
+			printf("%d", x);
+			
+		}
+	
+}
