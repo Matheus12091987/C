ee1533b2f7684bad8fec392a56d633b86bbbc4c9
Matheus <matheus.peres01@fatec.sp.gov.br>
2018-05-17 21:46:02 -0300

    Organização dos Arquivos

--- Exemplos_Aula_C/Leticia.c
+++ Exemplos_Aula_C/Leticia.c
@@ -1,17 +1,48 @@
-#include <stdio.h>
-#include <stdlib.h>
-//#include <time.h>
+/*	1ª Lista de exercicios de LP - Exercicio numero 1
 
-int i, x;
+	Nome: Matheus Gonçalves Peres
+	RA: 2840481723024									*/
+
+#include <stdio.h>
+#include <stdlib.h
+
+void regr(int n){
+
+	printf("%d\n", n);
+	n = n-2;
+
+	if (n<=0){
+
+		printf("0");
+		return 0;
+
+	}
+	else{
+
+		return (regr(n));
+
+	}
+
+}
+
+void bin(int n){
+
+
+	return (0);
+}
+
 
 void main(void){
 	
-		srand(time(NULL));
-		for(;x;){
-			
-			x= (rand()%11)-5;
-			printf("%d", x);
-			
-		}
-	
+	int n = 0;
+
+	printf("Digite um numero: ");
+	scanf("%d", &n);
+	system("clear");
+
+	regr(n);
+	bin(n);
+
+	return(0);
 }
+
